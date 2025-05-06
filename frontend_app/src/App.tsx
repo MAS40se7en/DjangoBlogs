717ee80579f2e1c19a9da861a7919d738fe8b28c
MAS40se7en <manir.saker@gmail.com>
2025-05-06 18:02:52 +0200

    ShadCN added to the frontend UI

--- frontend_app/src/App.tsx
+++ frontend_app/src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css'
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <>
       <div>
+        <Button className="text-black">
+          button
+        </Button>
         <h1 className='text-3xl font-bold underline'>BLOGS</h1>
       </div>
     </>
